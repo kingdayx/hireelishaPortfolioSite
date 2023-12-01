23298951cb9f0d5e3385e1c7f5dc4d37714f5f55
kingdayx <elishaday989@gmail.com>
2023-12-01 05:37:50 -0500

    updated and styled header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,9 +5,29 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
-      <div>
-        <SocialIcon url="https://www.github.com/kingdayx" />{" "}
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto">
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="https://www.github.com/kingdayx"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://www.linkedin.com/in/elisha-day-jenkins-64b1b6132/"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div className="flex flex-row items-center cursor-pointer text-gray-300">
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-400">
+          Get In Touch
+        </p>
       </div>
     </header>
   );
