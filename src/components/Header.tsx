70cfdf1ec1d02279e26f5b719478445b155e069f
kingdayx <elishaday989@gmail.com>
2023-12-01 06:07:36 -0500

    added animations to first section of header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header className="sticky top-0 flex flex-row items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
-      <div className="flex flex-row items-center">
+    <header className="sticky top-0 flex items-start justify-between max-w-7xl mx-auto z-20 xl:items-center p-5">
+      <motion.div
+        initial={{ x: -500, opacity: 0, scale: 0.5 }}
+        animate={{ x: 0, opacity: 1, scale: 1 }}
+        className="flex flex-row items-center"
+      >
         <SocialIcon
           url="https://www.github.com/kingdayx"
           fgColor="gray"
@@ -17,7 +22,7 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
       <div className="flex flex-row items-center cursor-pointer text-gray-300">
         <SocialIcon
           className="cursor-pointer"
